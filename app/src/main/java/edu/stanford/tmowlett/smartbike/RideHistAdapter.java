e9767dc6c8b0f54fe3ab672fff6973a09e699839
Tom Owlett <tomowlett@gmail.com>
2016-04-12 10:53:36 -0700

    Lab 2 Checkoff Code

--- app/src/main/java/edu/stanford/tmowlett/smartbike/RideHistAdapter.java
+++ app/src/main/java/edu/stanford/tmowlett/smartbike/RideHistAdapter.java
@@ -8,25 +8,26 @@ import android.widget.BaseAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class RideHistAdapter extends BaseAdapter {
 
+    // declare instance variables
     private Context context;
-    private String[] ridelocs;
-    private String[] ridedates;
-    private int[] rideicons;
+    private ArrayList<RideHistoryActivity.RideInfo> rides;
 
-    public RideHistAdapter(Context contextin, String[] ridelocsin, String[] ridedatesin, int[] iconsin){
-        context = contextin;
-        ridelocs = ridelocsin;
-        ridedates = ridedatesin;
-        rideicons = iconsin;
+    //Constructor to set instance variables
+    public RideHistAdapter(Context contextIn, ArrayList<RideHistoryActivity.RideInfo> rideInfoIn){
+        context = contextIn;
+        rides = rideInfoIn;
     }
 
     @Override
     public int getCount() {
-        return ridelocs.length;
+        return rides.size();
     }
 
+    //Get current position
     @Override
     public Object getItem(int position) {
         return position;
@@ -39,6 +40,7 @@ public class RideHistAdapter extends BaseAdapter {
 
     @Override
     public View getView(int pos, View cV, ViewGroup par) {
+        //Get row from existing view
         View row = cV;
 
         //If the row currently doesn't exist then create it
@@ -47,7 +49,7 @@ public class RideHistAdapter extends BaseAdapter {
             LayoutInflater in = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             row = in.inflate(R.layout.ride_list, par, false);
 
-            //create new viewholder
+            //create new viewholder to hold ui elements
             ViewHolder holder = new ViewHolder();
             holder.rideloc = (TextView)row.findViewById(R.id.location_tv);
             holder.ridedate = (TextView)row.findViewById(R.id.date_tv);
@@ -55,13 +57,15 @@ public class RideHistAdapter extends BaseAdapter {
             row.setTag(holder);
         }
 
+        //Set text and picture of viewholder object
         ViewHolder h = (ViewHolder)row.getTag();
-        h.rideloc.setText(ridelocs[pos]);
-        h.ridedate.setText(ridedates[pos]);
-        h.rideicon.setImageResource(rideicons[pos]);
+        h.rideloc.setText(rides.get(pos).getRideLocation());
+        h.ridedate.setText(rides.get(pos).getRideDate());
+        h.rideicon.setImageResource(rides.get(pos).getRideIcon());
         return row;
     }
 
+    //Viewholder object to hold ui elements
     static class ViewHolder {
         public TextView rideloc;
         public TextView ridedate;
--- app/src/main/java/edu/stanford/tmowlett/smartbike/RideHistoryActivity.java
+++ app/src/main/java/edu/stanford/tmowlett/smartbike/RideHistoryActivity.java
@@ -3,10 +3,12 @@ package edu.stanford.tmowlett.smartbike;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
+
+import com.gc.materialdesign.views.ButtonFloat;
+import com.gc.materialdesign.views.ButtonRectangle;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -14,36 +16,82 @@ import java.util.Date;
 
 public class RideHistoryActivity extends AppCompatActivity {
 
+    //Declare instance variables
     ListView rideListView;
-    Button addRideButton;
+    ButtonRectangle addRideButton;
     EditText rideLocEditText;
 
-    String[] ride_locs = {"Stanford", "PA", "Oakland", "Thadland", "Candyland"};
-    String[] ride_dates = {"Today", "Tomorrow", "Yesterday", "Today", "Yesterday"};
-    int[] ride_icons = {R.drawable.rb, R.drawable.mb, R.drawable.rb, R.drawable.rb, R.drawable.mb};
-    //ArrayList<String> ride_locs = new ArrayList<String>();
+    //Create Arraylist of rides
+    ArrayList<RideInfo> rides = new ArrayList<RideInfo>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ride_history);
 
+        //Get UI Elements
         rideListView = (ListView)findViewById(R.id.ride_listview);
-        addRideButton = (Button)findViewById(R.id.add_ride_button);
+        addRideButton = (ButtonRectangle)findViewById(R.id.add_ride_button);
         rideLocEditText = (EditText)findViewById(R.id.ride_edittext);
 
-        //final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, android.R.id.text1, ride_locs);
-        //rideListView.setAdapter(adapter);
+        //Add some dummy rides
+        rides.add(new RideInfo("Stanford", "04/05/2016", R.drawable.rb));
+        rides.add(new RideInfo("Palo Alto", "04/06/2016", R.drawable.mb));
 
-        final RideHistAdapter adapter = new RideHistAdapter(this, ride_locs, ride_dates, ride_icons);
+        //Create ride history adapter and assign to listview
+        final RideHistAdapter adapter = new RideHistAdapter(this, rides);
         rideListView.setAdapter(adapter);
 
+        //Create click listener and handle click
         addRideButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String date = new SimpleDateFormat("MM/dd/yyyy").format(new Date());
+                //Get the location from edittext and date from the system
+                String curLoc = rideLocEditText.getText().toString();
+                String curDate = new SimpleDateFormat("MM/dd/yyyy").format(new Date());
+
+                //Random icon assigned
+                int rideType;
+                if ((int)(Math.random()*2) == 0) {
+                    rideType = R.drawable.mb;
+                } else {
+                    rideType = R.drawable.rb;
+                }
+
+                //Push new RideInfo object to list and notify adapter
+                rides.add(new RideInfo(curLoc, curDate, rideType));
                 adapter.notifyDataSetChanged();
+                Toast.makeText(RideHistoryActivity.this, R.string.ride_added_message, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    // object to store units of ride information
+    public class RideInfo {
+
+        //instance variables
+        private String rideLocation;
+        private String rideDate;
+        private int rideIcon;
+
+        //Constructor to set instance variables
+        public RideInfo(String location_in, String date_in, int icon_in){
+            rideLocation = location_in;
+            rideDate = date_in;
+            rideIcon = icon_in;
+        }
+
+        //Getter functions for each instance variable
+        public String getRideLocation(){
+            return rideLocation;
+        }
+
+        public String getRideDate(){
+            return rideDate;
+        }
+
+        public int getRideIcon(){
+            return rideIcon;
+        }
     }
 }
